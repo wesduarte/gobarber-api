d29ba1e88ce8c45973d193d7254d1b9591c55f23
Wesley <wesleydmuniz@outlook.com>
2020-08-31 08:58:19 -0300

    Setting up mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import databaseConfig from '../config/database';
@@ -16,6 +17,17 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      },
+    );
   }
 }
 export default new Database();
