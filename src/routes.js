79218040242aef6c2150ff75fa7ba1d78e1e63ea
Wesley <wesleydmuniz@outlook.com>
2020-08-31 14:04:24 -0300

    Marking read notifications as read

--- src/routes.js
+++ src/routes.js
@@ -29,5 +29,6 @@ routes.get('/schedules', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
