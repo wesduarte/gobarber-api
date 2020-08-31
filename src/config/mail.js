64f04d007b8383be919337605243c9dde7d009a2
Wesley <wesleydmuniz@outlook.com>
2020-08-31 15:51:48 -0300

    Notifying deleted appointments by email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '418b76f31fa0f4',
+    pass: '96b285ba505b00',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
