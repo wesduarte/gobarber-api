664d22a64b26a217e9b74ae696b6489dc688143a
Wesley <wesleydmuniz@outlook.com>
2020-08-31 00:13:35 -0300

    Adding field validation to session and user

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,22 +1,32 @@
+import * as Yup from 'yup';
 import jwt from 'jsonwebtoken';
+import authConf from '../../config/auth';
 import User from '../models/User';
-
-import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
-
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
+      return res.status(401).json({ error: 'Password does not match!' });
     }
-    
+
     const { id, name } = user;
 
     return res.json({
@@ -25,8 +35,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expireIn,
+      token: jwt.sign({ id }, authConf.secret, {
+        expiresIn: authConf.expireIn,
       }),
     });
   }
