7ac77ad9ff601b51fba3d2f8e478478da3540fa9
Wesley <wesleydmuniz@outlook.com>
2020-08-31 02:36:11 -0300

    Adding appointments creation resource

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import UserController from './app/controllers/UserController';
+import AppointmentController from './app/controllers/AppointmentController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
@@ -17,6 +18,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
