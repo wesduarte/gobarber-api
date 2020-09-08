50a731c3ec591d31ca77c94004a1b624c1ed2676
Wesley <wesleydmuniz@outlook.com>
2020-09-08 15:23:04 -0300

    Creating Cancel Appointment Service

---
+++ src/app/services/CancelAppointmentService.js
@@ -0,0 +1,45 @@
+import { isBefore, subHours } from 'date-fns';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class CancelAppointmentService {
+  async run({ provider_id, user_id }) {
+    const appointment = await Appointment.findByPk(provider_id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== user_id) {
+      throw new Error("You don't have permission to cancel this appointment.");
+    }
+
+    // removo duas horas da data agendada
+    const dateWithSub = subHours(appointment.date, 2);
+    const NOW = new Date();
+    if (isBefore(dateWithSub, NOW)) {
+      throw new Error('You can only cancel appointment 2 hours in advance.');
+    }
+
+    appointment.canceled_at = NOW;
+
+    await appointment.save();
+
+    await Queue.add(CancellationMail.key, { appointment });
+
+    return appointment;
+  }
+}
+
+export default new CancelAppointmentService();
